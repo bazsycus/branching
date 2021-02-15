4ca4c02f4e1d0facba4f45b7ab00595ac876be30
bazsycus <62937986+bazsycus@users.noreply.github.com>
2021-02-15 17:11:32 +0100

    Add files via upload

---
+++ branching/3_Elagazsok/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace _3_Elagazsok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 29);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 263);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Eredmény";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 75);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 151);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Páros-e?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(314, 75);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(270, 151);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Érdemjegy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(616, 75);
+            this.button3.Margin = new System.Windows.Forms.Padding(6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(270, 151);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Érdemjegy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(445, 347);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 29);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(664, 347);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(180, 29);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(475, 317);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Életkor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(682, 317);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Szolgálati idő(év)";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(252, 317);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(65, 28);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Férfi";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(252, 351);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(53, 28);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.Text = "Nő";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(421, 388);
+            this.button4.Margin = new System.Windows.Forms.Padding(6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(270, 151);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Nyugdíjas";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1161, 591);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ branching/3_Elagazsok/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_Elagazsok
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int iBeolvas = int.Parse(textBox1.Text);
+            /*  egyenlő ==
+             *  nem egyenlő   !=
+             *  <,>,<=,>=
+             * */
+            
+            if (iBeolvas%2==0) //szám%szám2, a szám a szám2-vel képzett maradéka
+            {
+                //igaz ág
+                label1.Text = "Páros";
+            }
+            else
+            {
+                //hamis ág
+                label1.Text = "Páratlan";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int iPontszam = int.Parse(textBox1.Text);
+            if (iPontszam<40)
+            {
+                label1.Text = "Elégtelen";
+            }
+            else if (iPontszam<55)
+            {
+                label1.Text = "Görbül";
+            }
+            else if (iPontszam<70)
+            {
+                label1.Text = "Közepes";
+            }
+            else if (iPontszam<85)
+            {
+                label1.Text = "Jó";
+            } else
+            {
+                label1.Text = "Stréber";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int iJegy = int.Parse(textBox1.Text);
+            switch (iJegy)
+            {
+                case 1:
+                    label1.Text = "Elégtelen";
+                    break;
+                case 2:
+                    label1.Text = "Elégséges";
+                    break;
+                case 3:
+                    label1.Text = "Közepes";
+                    break;
+                case 4:
+                    label1.Text = "Jó";
+                    break;
+                case 5:
+                    label1.Text = "Jeles";
+                    break;
+                default:
+                    label1.Text = "Szabálytalan jegy";
+                    break;
+            }
+        }
+        /*A feltétel (igaz/hamis)
+         * B feltétel (igaz/hamis)
+         * A és B --> A igaz és B igaz akkor A és B igaz
+         * és--> && altgr + c
+         * A vagy B ..> AKKOr lesz igaz ha legalább az egyik igaz
+         * vagy --> || alt gr + W
+         * 
+         * A && B megnézi hogy A igaz ha igaz megnézi hogy B igaz -->eredményt ad
+         * Ha A hamis B-t már nem nézi meg
+         * A & B  megnézi, hogy A igaz és megnézi hogy B igaz független A-tól
+         */
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int iKor = int.Parse(textBox2.Text);
+            int iSzol = int.Parse(textBox3.Text);
+            //radioButton1.Checked-->bool logikai változó
+            //ha be van jelölve akkor igaz különben hamis
+            bool bNő = radioButton2.Checked;
+            /*if (radioButton2.Checked) // ==true
+            {//if (!radioButton1.Checked){ == false
+                bNő = true;
+            } else
+            {
+                bNő = false;
+            }*/
+
+            if ( (iKor>=65) || ((bNő) && (iSzol>=40))   )
+            {
+                label1.Text = "Nyugdíjas";
+            }
+            else
+            {
+                label1.Text = "Nem nyugdíjas";
+            }
+        }
+    }
+}
